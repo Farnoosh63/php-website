e2954c4b7de52c59afcdd5c070af7ff1b67f13d3
Farnoosh <farnoosh@pdx.edu>
2017-02-03 14:00:06 -0800

    add correct timezone

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'vendor/autoload.php';
+date_default_timezone_set('America/Los_Angeles');
 
 $log = new Monolog\Logger('name');
 $log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
