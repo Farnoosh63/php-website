d595d1aa3d998f2aaa46cc1753c61f4b99edee14
Farnoosh <farnoosh@pdx.edu>
2017-02-03 13:54:23 -0800

    add monolog autoloading

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 
 require 'vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
+
+$log->addWarning('Foo');
+
 echo "hello word"; ?>
