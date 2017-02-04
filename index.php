6293fcef6a734df449f7d75bc9b8c3246359dd34
Farnoosh <farnoosh@pdx.edu>
2017-02-03 16:42:00 -0800

    adding dynamic url

--- index.php
+++ index.php
@@ -2,12 +2,19 @@
 require 'vendor/autoload.php';
 date_default_timezone_set('America/Los_Angeles');
 
-use Monolog\logger;
-use Monolog\Handler\StreamHandler;
+// use Monolog\logger;
+// use Monolog\Handler\StreamHandler;
+//
+// $log = new Logger('name');
+// $log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
+//
+// $log->addWarning('Oh noes');
 
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
+$app = new \Slim\Slim();
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+$app->run();
 
-$log->addWarning('Oh noes');
-
-echo "hello word"; ?>
+// echo "hello word";
+?>
